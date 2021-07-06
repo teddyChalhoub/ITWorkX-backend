0a9128b7f8d77eb7c5220f4d0338e89db676bedb
teddyChalhoub <teddychalhoub0@gmail.com>
2021-07-06 10:11:56 +0300

    adding controllers and models folders

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import mongoose from "mongoose";
 import { connectDB } from "./db";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+app.use(express.cors());
 
 const dbConnection = async () => {
     await connectDB();
