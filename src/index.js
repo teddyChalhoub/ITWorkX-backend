5bdc2ee42458970e05a591bbb94a4e3aa467b306
teddyChalhoub <teddychalhoub0@gmail.com>
2021-08-02 19:00:39 +0300

    test5

--- src/index.js
+++ src/index.js
@@ -28,11 +28,13 @@ app.use("/service", service);
 app.use("/NodeMailer", NodeMailer);
 
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, "../client/build")));
+app.use(express.static(path.resolve(__dirname, "../ITWorkX-frontend/build")));
 
 // All other GET requests not handled before will return our React app
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+  res.sendFile(
+    path.resolve(__dirname, "../ITWorkX-frontend/build", "index.html")
+  );
 });
 
 const dbConnection = async () => {
